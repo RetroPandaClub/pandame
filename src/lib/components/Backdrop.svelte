802c7e0de908b240760e7a7ed54cb2307751a09c
Antonio Ventilii <antonioventilii@gmail.com>
2024-11-02 07:41:34 +0100

    Initial commit.

---
+++ src/lib/components/Backdrop.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	interface Props {
+		spinner?: boolean;
+	}
+
+	let { spinner = false }: Props = $props();
+</script>
+
+<div
+	class:backdrop-blur-sm={spinner}
+	class:backdrop-blur-xl={!spinner}
+	class="fixed inset-0 z-40 bg-white/30 flex items-center justify-center"
+>
+	{#if spinner}
+		<div
+			class="w-12 h-12 rounded-full animate-spin border-[3px] border-solid border-lavender-blue-600 border-t-transparent"
+		></div>
+	{/if}
+</div>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	import { initSatellite } from '@junobuild/core-peer';
+	import Footer from '$lib/components/Footer.svelte';
+	import Background from '$lib/components/Background.svelte';
+	import '../app.css';
+	import Auth from '$lib/components/Auth.svelte';
+
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+
+	const init = async () => {
+		await initSatellite({
+			workers: {
+				auth: true
+			}
+		});
+	};
+
+	$effect(() => {
+		init();
+	});
+</script>
+
+<div class="relative isolate min-h-[100dvh]">
+	<main class="mx-auto max-w-screen-2xl py-16 px-8 md:px-24 tall:min-h-[calc(100dvh-128px)]">
+		<h1 class="dark:text-white text-5xl md:text-6xl font-bold tracking-tight md:pt-24">
+			Example App
+		</h1>
+		<p class="dark:text-white py-4 md:max-w-lg">
+			Explore this demo app built with SvelteKit, Tailwind, and
+			<a href="https://juno.build" rel="noopener noreferrer" target="_blank" class="underline">
+				Juno</a
+			>, showcasing a practical application of these technologies.
+		</p>
+
+		<Auth>
+			{@render children()}
+		</Auth>
+	</main>
+
+	<Footer />
+
+	<Background />
+</div>
