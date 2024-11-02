46ffe8f29863f6b4cb90aef9d82a1f52dbcab7b3
Antonio Ventilii <antonioventilii@gmail.com>
2024-11-02 21:54:45 +0100

    build: Juno config

---
+++ juno.config.ts
@@ -0,0 +1,11 @@
+import {defineConfig} from '@junobuild/config';
+
+export default defineConfig({
+  satellite: {
+    id: 'wqhtf-fqaaa-aaaal-amssq-cai',
+    source: 'build'
+  },
+  orbiter: {
+    id: 'gfpjj-5iaaa-aaaal-amr4a-cai'
+  }
+});
