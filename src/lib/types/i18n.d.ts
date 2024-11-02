70fd4955987adc3fe0cb4fefbc500c9459d4cfe1
Antonio Ventilii <antonioventilii@gmail.com>
2024-11-02 22:29:06 +0100

    feat: i18n

---
+++ src/lib/types/i18n.d.ts
@@ -0,0 +1,28 @@
+/**
+ * Auto-generated definitions file ("npm run i18n")
+ */
+
+interface I18nCore {
+	text: {
+		cancel: string;
+		next: string;
+		save: string;
+		back: string;
+		done: string;
+		close: string;
+		refresh: string;
+		name: string;
+		symbol: string;
+		decimals: string;
+		amount: string;
+		max: string;
+		more: string;
+		reject: string;
+		approve: string;
+	};
+}
+
+interface I18n {
+	lang: Languages;
+	core: I18nCore;
+}
